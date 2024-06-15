418d62f454efad5017c8c2dedce0aecd4ad608d8
Ayrca <ayrtoncarli@gmail.com>
2024-06-14 21:01:56 -0300

    front

--- clase_13/obligatorio.ts
+++ clase_13/obligatorio.ts
@@ -1,6 +1,6 @@
 import * as fs from 'node:fs';
-const precios: number[] = [525, 3500, 400, 1999]; const productos: string[] = ["Leche", "Galletitas", "Harina",
-    "Queso"]
+const precios: number[] = [525, 3500, 400, 1999];
+const productos: string[] = ["Leche", "Galletitas", "Harina", "Queso"]
 const filepathProductos = "./productos.txt"
 const filepathPrecios = "./precios.txt"
 writeTxt(productos, filepathProductos);
@@ -9,15 +9,11 @@ readTxt(filepathProductos);
 readTxt(filepathPrecios);
 
 
-function writeTxt(arreglo: number[] | string[], filepath: string) {
-    let contenido: string = crearString(arreglo);
-    fs.writeFileSync(filepath, contenido)
-}
 function crearString(arreglo: any[]): string {
     let contenido: string = "";
     for (let i: number = 0; i < arreglo.length; i++) {
         contenido += `${arreglo[i]} `;
-        /*let contenido = arreglo.join(" ");*/
+        /* let contenido = arreglo.join(" ");*/
     }
     return contenido
 }
@@ -25,6 +21,10 @@ function acomodarStr(cadena: string): any {
     const datos2: string = cadena.trim();//saca los espacios de adelante y de atras de la cadena de caracteres
     const nuevoArray: string[] = datos2.split(" ");
     console.log(nuevoArray);
+}
+function writeTxt(arreglo: number[] | string[], filepath: string) {
+    let contenido: string = crearString(arreglo);
+    fs.writeFileSync(filepath, contenido)
 }
 function readTxt(filepath: string): any {
     let datos: string = fs.readFileSync(filepath, 'utf8');
